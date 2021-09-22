627272199e41e5a6413c8ce9ff7837c45871c0d6
Andrew Hillen <andrew.m.hillen@gmail.com>
2021-09-21 19:52:29 -0600

    Refactor Followers.

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/FollowService.java
@@ -10,13 +10,17 @@ import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import edu.byu.cs.tweeter.client.backgroundTask.GetFollowersTask;
 import edu.byu.cs.tweeter.client.backgroundTask.GetFollowingTask;
+import edu.byu.cs.tweeter.client.cache.Cache;
+import edu.byu.cs.tweeter.client.view.main.followers.FollowersFragment;
 import edu.byu.cs.tweeter.model.domain.AuthToken;
 import edu.byu.cs.tweeter.model.domain.User;
 
 public class FollowService
 {
 
+    // GetFollowing portions ------------------------------------------------
     public interface GetFollowingObserver
     {
         void getFollowingSuccess(List<User> user, boolean hasMorePages, User lastFollowee);
@@ -66,6 +70,60 @@ public class FollowService
             {
                 Exception ex = (Exception) msg.getData().getSerializable(GetFollowingTask.EXCEPTION_KEY);
                 observer.getFollowingException(ex);
+            }
+        }
+    }
+
+    // GetFollower portions --------------------------------------------------
+    public interface GetFollowerObserver
+    {
+        void getFollowerSuccess(List<User> user, boolean hasMorePages, User lastFollower);
+        void getFollowerFailure(String message);
+        void getFollowerException(Exception ex);
+    }
+
+    public void getFollowers(AuthToken authToken, User target, int limit, User lastFollower, GetFollowerObserver observer)
+    {
+        GetFollowersTask getFollowersTask = new GetFollowersTask(authToken, target, limit, lastFollower,
+                new GetFollowersHandler(observer));
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(getFollowersTask);
+    }
+
+    /**
+     * Message handler (i.e., observer) for GetFollowersTask.
+     */
+    private class GetFollowersHandler extends Handler {
+
+        private GetFollowerObserver observer;
+
+        public GetFollowersHandler(GetFollowerObserver observer)
+        {
+            this.observer = observer;
+        }
+
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            boolean success = msg.getData().getBoolean(GetFollowersTask.SUCCESS_KEY);
+            if (success)
+            {
+                List<User> followers = (List<User>) msg.getData().getSerializable(GetFollowersTask.FOLLOWERS_KEY);
+                boolean hasMorePages = msg.getData().getBoolean(GetFollowersTask.MORE_PAGES_KEY);
+                User lastFollower = (followers.size() > 0) ? followers.get(followers.size() - 1) : null;
+
+                observer.getFollowerSuccess(followers, hasMorePages, lastFollower);
+
+                //followersRecyclerViewAdapter.addItems(followers);
+            }
+            else if (msg.getData().containsKey(GetFollowersTask.MESSAGE_KEY))
+            {
+                String message = msg.getData().getString(GetFollowersTask.MESSAGE_KEY);
+                observer.getFollowerFailure("Failed to get followers: " + message);
+            }
+            else if (msg.getData().containsKey(GetFollowersTask.EXCEPTION_KEY))
+            {
+                Exception ex = (Exception) msg.getData().getSerializable(GetFollowersTask.EXCEPTION_KEY);
+                observer.getFollowerException(ex);
             }
         }
     }
---
+++ app/src/main/java/edu/byu/cs/tweeter/client/presenter/FollowerPresenter.java
@@ -0,0 +1,102 @@
+package edu.byu.cs.tweeter.client.presenter;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.client.model.service.FollowService;
+import edu.byu.cs.tweeter.client.model.service.UserService;
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public class FollowerPresenter implements FollowService.GetFollowerObserver, UserService.Observer
+{
+
+    public interface View
+    {
+        void addItems(List<User> followers);
+        void setLoading(boolean isLoading);
+        void navigateToUser(User user);
+
+        void displayErrorMessage(String message);
+        void displayInfoMessage(String message);
+
+    }
+
+    private FollowerPresenter.View view;
+    private AuthToken authToken;
+    private User user;
+    private User lastFollower;
+    private boolean isLoading = false;
+
+    private boolean hasMorePages = true;
+
+    private static final int PAGE_SIZE = 10;
+
+    public FollowerPresenter(FollowerPresenter.View view, AuthToken authToken, User user)
+    {
+        this.view = view;
+        this.authToken = authToken;
+        this.user = user;
+    }
+
+    public void loadMoreItems()
+    {
+        if(!isLoading && hasMorePages)
+        {
+            isLoading = true;
+            view.setLoading(true);
+            new FollowService().getFollowers(authToken, user, PAGE_SIZE, lastFollower, this);
+        }
+    }
+
+    public void goToUser(String alias)
+    {
+        view.displayInfoMessage("Getting user's profile...");
+        new UserService().getUser(authToken, alias, this);
+    }
+
+    @Override
+    public void getFollowerSuccess(List<User> followers, boolean hasMorePages, User lastFollower)
+    {
+        this.lastFollower = lastFollower;
+        isLoading = false;
+        view.setLoading(false);
+        view.addItems(followers);
+        this.hasMorePages = hasMorePages;
+    }
+
+    @Override
+    public void getFollowerFailure(String message)
+    {
+        isLoading = false;
+        view.setLoading(false);
+        view.displayErrorMessage(message);
+    }
+
+    @Override
+    public void getFollowerException(Exception ex)
+    {
+        isLoading = false;
+        view.setLoading(false);
+        // Do exception stuff
+    }
+
+    // UserService overrides ----------------------------------
+
+    @Override
+    public void getUserSuccess(User user)
+    {
+        view.navigateToUser(user);
+    }
+
+    @Override
+    public void getUserFailure(String message)
+    {
+        view.displayErrorMessage(message);
+    }
+
+    @Override
+    public void getUserException(Exception ex)
+    {
+        //Do Exception stuff
+    }
+}
