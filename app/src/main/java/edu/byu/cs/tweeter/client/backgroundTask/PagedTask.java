01a67d712d68174e69f3c942e5ca96967d576053
Andrew Hillen <andrew.m.hillen@gmail.com>
2021-10-05 22:00:28 -0600

    Refactor User Paging.

---
+++ app/src/main/java/edu/byu/cs/tweeter/client/backgroundTask/PagedTask.java
@@ -0,0 +1,51 @@
+package edu.byu.cs.tweeter.client.backgroundTask;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import java.util.List;
+
+import edu.byu.cs.tweeter.model.domain.AuthToken;
+import edu.byu.cs.tweeter.model.domain.User;
+
+public abstract class PagedTask<T> extends AuthenticatedTask
+{
+
+    public static final String MORE_PAGES_KEY = "more-pages";
+
+    /**
+     * The user whose followers are being retrieved.
+     * (This can be any user, not just the currently logged-in user.)
+     */
+    protected User targetUser;
+    /**
+     * Maximum number of followers to return (i.e., page size).
+     */
+    protected int limit;
+    /**
+     * The last follower returned in the previous page of results (can be null).
+     * This allows the new page to begin where the previous page ended.
+     */
+    protected T lastItem;
+
+    protected boolean hasMorePages;
+
+    protected List<T> items;
+
+    public PagedTask(Handler messageHandler, AuthToken authToken, User targetUser, int limit, T lastItem)
+    {
+        super(messageHandler, authToken);
+        this.targetUser = targetUser;
+        this.limit = limit;
+        this.lastItem = lastItem;
+    }
+
+    protected abstract void addItemsToBundle(Bundle msgBundle);
+
+    @Override
+    protected void loadSuccessBundle(Bundle msgBundle)
+    {
+        addItemsToBundle(msgBundle);
+        msgBundle.putBoolean(MORE_PAGES_KEY, hasMorePages);
+    }
+}
